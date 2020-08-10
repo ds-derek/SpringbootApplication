c77b2f0e66f6e83cb4f9d20f8da6676c990af20d
ids1207 <dieske@naver.com>
2020-08-10 16:10:30 +0900

    add serialVersionUID

--- src/main/java/com/example/app/exceptions/DuplicateAccountException.java
+++ src/main/java/com/example/app/exceptions/DuplicateAccountException.java
@@ -1,6 +1,9 @@
 package com.example.app.exceptions;
 
 public class DuplicateAccountException extends RuntimeException{
+
+    private static final long serialVersionUID = 1450011509252993373L;
+
     public DuplicateAccountException(String account){
         super(account + " is duplicated.");
     }
--- src/main/java/com/example/app/exceptions/NoEntityFoundException.java
+++ src/main/java/com/example/app/exceptions/NoEntityFoundException.java
@@ -1,6 +1,8 @@
 package com.example.app.exceptions;
 
 public class NoEntityFoundException extends RuntimeException {
+    private static final long serialVersionUID = -8059812811968886319L;
+
     public NoEntityFoundException(String entity, String by){
         super("Could not found Resource "+entity+" by "+by);
     }
